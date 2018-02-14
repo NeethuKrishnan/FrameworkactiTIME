1b48824476349a21ec845ffeba546b0f76812519
bavooty <bavooty@DESKTOP-2O7KULO>
2018-02-14 11:54:44 +0530

    First commit

---
+++ src/Example/Execution.java
@@ -0,0 +1,17 @@
+package Example;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Execution
+
+{
+	@Test
+public void testB()
+{
+		
+	Reporter.log("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiii",true);	
+		}
+	
+	
+}
